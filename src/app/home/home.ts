408e5604f0cdcc594ad7638eeab7b7f1ca270440
ArtthurRibbeiro <art.rib@hotmail.com>
2026-09-24 14:39:28 -0300

    primeira versão portifolio do lucas

---
+++ src/app/home/home.ts
@@ -0,0 +1,76 @@
+import { Component } from '@angular/core';
+import { $ } from 'jquery';
+import Swiper from 'swiper';
+import 'swiper/css';
+import 'swiper/css/autoplay';
+import 'swiper/css/effect-coverflow';
+import { Autoplay, EffectCoverflow } from 'swiper/modules';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  imports: [RouterLink],
+  selector: 'app-home',
+  styleUrl: './home.css',
+  templateUrl: './home.html',
+})
+export class Home {
+
+  inicialize(callback?: () => void){
+    $(".background-filter").fadeOut(500);
+    $('.backgroung-pic').css({
+      'transition': 'background-size 0.8s ease',
+      'background-size': '120%'
+    });
+    $('.about-me').css('display', 'flex').hide().fadeIn(500);
+    $('.projects').fadeIn(500, () => { 
+      callback ?.();
+
+    });
+
+    this.criaCarrosselMarquee("#marquee1", false)
+    this.criaCarrosselMarquee("#marquee2", true)
+
+
+  }
+
+  rolarParaProjetos() {
+    $('html, body').scrollTop($('.projects').offset()!.top);
+  }
+
+
+
+
+  criaCarrosselMarquee(seletor: string, isReverse: boolean){
+    const swiper = new Swiper(seletor, {
+     modules: [Autoplay, EffectCoverflow],
+     slidesPerView:'auto',
+     spaceBetween: 50,
+     loop: true,
+     speed: 10000,
+     allowTouchMove: true,
+     autoplay:{
+       delay: 0,
+       disableOnInteraction: false,
+       reverseDirection: isReverse,
+     },
+     effect: 'coverflow',
+     coverflowEffect: {
+      rotate: 10,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      scale: 0.95,
+      slideShadows: false,
+     }
+   
+   });
+
+  }
+  
+  ngAfterViewInit(){
+   }
+
+  
+  
+
+}
